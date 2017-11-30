e8d985edfe53dc0cf0639d7aba109317b0899868
hokhim530 <hokhim530Hok.Lai.2014@live.rhul.ac.uk>
2017-11-30 11:34:45 +0000

    Added reduce function and Lovasz condition to LLL with unittest.

--- src/Test/TestLLL.py
+++ src/Test/TestLLL.py
@@ -15,3 +15,17 @@ class Test(unittest.TestCase):
         result = lll.Swap(basis, 1, 2)
         pResult = [[15, 23, 11], [-14, -14, -2], [31, -8, -8]]
         self.assertListEqual(result, pResult)
+        
+    def testLovaszCondition(self):
+        lll = LLLAlgorithm()
+        basis = [[15, 23, 11], [1, 9, 9], [31, -8, -8]]
+        result = lll.LovaszCondition(basis, 1)
+        pResult = [[1, 9, 9], [15, 23, 11], [31, -8, -8]]
+        self.assertListEqual(result, pResult)
+        
+    def testReduce(self):
+        lll = LLLAlgorithm()
+        basis = [[1, 9, 9], [15, 23, 11], [31, -8, -8]]
+        result = lll.Reduce(basis, 1)
+        pResult = [[1, 9, 9], [13, 5, -7], [31, -8, -8]]
+        self.assertListEqual(result, pResult)
--- src/src/lll.py
+++ src/src/lll.py
@@ -3,9 +3,29 @@ Created on 20 October 2017
 
 @author: Lai Hok Him
 '''
+import numpy as np
+from src.gs import GramSchmidt
 
 class LLLAlgorithm(object):
     
     def Swap(self, basis, i, j):
         basis[i], basis[j] = basis[j], basis[i]
+        return basis
+    
+    def LovaszCondition(self, basis, i):
+        gs= GramSchmidt()
+        oBasis = gs.GS(basis)
+        v1 = np.dot(oBasis[i], oBasis[i])
+        v2 = ((3/4) - np.square(gs.GSCoef(oBasis[i - 1], basis[i]))) * np.dot(oBasis[i - 1], oBasis[i - 1])
+        if v1 < v2:
+            basis = self.Swap(basis, i, i -1)
+        return basis
+    
+    def Reduce(self, basis, i):
+        gs= GramSchmidt()
+        for j in range(i):
+            v1 = np.asarray(basis[i])
+            v2 = np.around(gs.GSCoef(np.asarray(basis[j]), v1))
+            result = v1 - gs.Multi(np.asarray(basis[j]), v2)
+            basis[i] = result.tolist()
         return basis
