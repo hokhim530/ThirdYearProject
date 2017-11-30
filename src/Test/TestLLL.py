1cee6a7a278ace3a833db366a501f089dfb3518d
hokhim530 <hokhim530Hok.Lai.2014@live.rhul.ac.uk>
2017-11-30 10:18:50 +0000

    Reconstruction to the LLL class and unittest.

--- src/Test/TestLLL.py
+++ src/Test/TestLLL.py
@@ -5,20 +5,13 @@ Created on 20 October 2017
 '''
 
 import unittest
-from src.lll import LLL
+from src.lll import LLLAlgorithm
 
 class Test(unittest.TestCase):
-
-    def testGSOB(self):
-        lll = LLL()
-        basis = [[1, 1, 1], [-1, 0, 2], [3, 5, 6]]
-        result = lll.GSOB(basis)
-        pResult = [[1, 1, 1], [-4/ 3, -1/ 3, 5/ 3], [-6/ 14, 9/ 14, -3/ 14]]
-        self.assertListEqual(result, pResult)
         
-    def testReduce(self):
-        lll = LLL()
-        basis = [[15, 23, 11], [46, 15, 3], [32, 1, 1]]
-        result = lll.Reduce(basis)
-        pResult = [[15, 23, 11], [31, -8, -8], [-14, -14, -2]]
+    def testSwap(self):
+        lll = LLLAlgorithm()
+        basis = [[15, 23, 11], [31, -8, -8], [-14, -14, -2]]
+        result = lll.Swap(basis, 1, 2)
+        pResult = [[15, 23, 11], [-14, -14, -2], [31, -8, -8]]
         self.assertListEqual(result, pResult)
--- src/src/lll.py
+++ src/src/lll.py
@@ -4,27 +4,8 @@ Created on 20 October 2017
 @author: Lai Hok Him
 '''
 
-import numpy as np
-from src.gs import GramSchmidt
-
-class LLL(object):
+class LLLAlgorithm(object):
     
-    def GSOB(self, basis):
-        gs = GramSchmidt()
-        return gs.GS(basis)
-    
-    def Reduce(self, basis):
-        rBasis = []
-        for i in range(len(basis)):
-            v1 = np.asarray(basis[i])
-            if i != 0:
-                rv = v1
-                for j in range(i):
-                    v2 = np.asarray(rBasis[j])
-                    rv = np.subtract(rv, v2)
-            else:
-                rv = v1
-            rBasis.append(rv.tolist())
-            print(rv)
-            print(rBasis)
-        return rBasis
+    def Swap(self, basis, i, j):
+        basis[i], basis[j] = basis[j], basis[i]
+        return basis
